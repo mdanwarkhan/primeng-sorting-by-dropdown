c0f97ef706a3ba636a47de91b9b40260d0eea953
mdanwarkhan <anwarkhan772@gmail.comn>
2019-11-23 16:53:13 +0530

    Toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   colName: string;
   order = false;
   myClass = true;
+  status: boolean = false;
   @Output() sortFunction: EventEmitter<any> = new EventEmitter<any>();
 
   ngOnInit() {
@@ -43,37 +44,37 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.sortTable.sortFunction = this.sortFunction;
   }
 
-  customSort(event: any) {
-    event.data.sort((data1, data2) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
-      let result = null;
+  // customSort(event: any) {
+  //   event.data.sort((data1, data2) => {
+  //     let value1 = data1[event.field];
+  //     let value2 = data2[event.field];
+  //     let result = null;
 
-      if (value1 == null && value2 != null) result = -1;
-      else if (value1 != null && value2 == null) result = 1;
-      else if (value1 == null && value2 == null) result = 0;
-      else if (typeof value1 === "string" && typeof value2 === "string") {
-        if (event.field === "quarter") {
-          console.log("sort..." + event.field);
-          let o1 = value1.split("-");
-          let o2 = value2.split("-");
-          let value1Q = o1.length == 2 ? o1[0] : "";
-          let value1Y = o1.length == 2 ? o1[1] : o1[0];
-          let value2Q = o2.length == 2 ? o2[0] : "";
-          let value2Y = o2.length == 2 ? o2[1] : o2[0];
-          if (value1Y.localeCompare(value2Y) === 0) {
-            result = value1Q.localeCompare(value2Q);
-          } else {
-            result = value1Y.localeCompare(value2Y);
-          }
-        } else {
-          result = value1.localeCompare(value2);
-        }
-      } else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+  //     if (value1 == null && value2 != null) result = -1;
+  //     else if (value1 != null && value2 == null) result = 1;
+  //     else if (value1 == null && value2 == null) result = 0;
+  //     else if (typeof value1 === "string" && typeof value2 === "string") {
+  //       if (event.field === "quarter") {
+  //         console.log("sort..." + event.field);
+  //         let o1 = value1.split("-");
+  //         let o2 = value2.split("-");
+  //         let value1Q = o1.length == 2 ? o1[0] : "";
+  //         let value1Y = o1.length == 2 ? o1[1] : o1[0];
+  //         let value2Q = o2.length == 2 ? o2[0] : "";
+  //         let value2Y = o2.length == 2 ? o2[1] : o2[0];
+  //         if (value1Y.localeCompare(value2Y) === 0) {
+  //           result = value1Q.localeCompare(value2Q);
+  //         } else {
+  //           result = value1Y.localeCompare(value2Y);
+  //         }
+  //       } else {
+  //         result = value1.localeCompare(value2);
+  //       }
+  //     } else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
 
-      return event.order * result;
-    });
-  }
+  //     return event.order * result;
+  //   });
+  // }
 
   mySort() {
     console.log(this.sortTable);
@@ -95,6 +96,11 @@ export class AppComponent implements OnInit, AfterViewInit {
   columnselection(ev) {
     console.log(ev);
     this.colName = ev;
+    // this.mySort();
+  }
+
+  clickEvent() {
+    this.status = !this.status;
     this.mySort();
   }
 }
